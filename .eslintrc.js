9176f37c0a6597ca336fc966cb40b0647af9b582
Vyacheslav B <la.sintez@gmail.com>
2019-01-16 12:57:21 +0300

    feat: bug initialisation

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    'plugin:vue/recommended'
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'vue', 'import'
+  ],
+  // add your custom rules here
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'import/no-unresolved': [
+      'error'
+    ]
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="container">
+    <div>
+      <img src="~/assets/example.svg">
+    </div>
+  </section>
+</template>
+
+<script>
+import rect from '~/assets/example.svg?inline'
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
